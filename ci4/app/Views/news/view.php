dbb37012bcb2e348f84e3774030a402f2ee11e56
JohnCedricCarl <jbmolina@student.apc.edu.ph>
2023-03-05 09:58:22 +0800

    more navigation

--- ci4/app/Views/news/view.php
+++ ci4/app/Views/news/view.php
@@ -1,11 +1,11 @@
 <div class="navbar">
     <ul>
-        <li><a href="#about">ABOUT</a></li>
-        <li><a href="#contact">CONTACT</a></li>
-        <li><a href="#resources">RESOURCES</a></li>
-        <li><a href="formvalidation">FORMS</a></li>
-        <li><a href="news/index">NEWS</a></li>
-        <li><a href="news/create">CREATE NEWS</a></li>
+        <li><a href="/pages/home#about">ABOUT</a></li>
+        <li><a href="/pages/home#contact">CONTACT</a></li>
+        <li><a href="/pages/home#resources">RESOURCES</a></li>
+        <li><a href="/pages/formvalidation">FORMS</a></li>
+        <li><a href="index">NEWS</a></li>
+        <li><a href="create">CREATE NEWS</a></li>
         <li><button type="button"
           onclick="document.getElementById('demo').innerHTML = Date()">
           What time is it?</button>
