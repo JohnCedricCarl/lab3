57e4214d259ef29f3e148ebe9ae8b74e64735cdd
JohnCedricCarl <jbmolina@student.apc.edu.ph>
2023-03-06 11:36:48 +0800

    visitors controller

--- ci4/app/Controllers/Visitors.php
+++ ci4/app/Controllers/Visitors.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Controllers;
+
 use App\Models\VisitorsModel;
+use CodeIgniter\Exceptions\PageNotFoundException;
 
 class Visitors extends BaseController
 {
@@ -10,24 +12,41 @@ class Visitors extends BaseController
         $model = model(VisitorsModel::class);
 
         $data = [
-            'visitors'  => $model->getVisitors(),
-            'title' => 'Visitors',
+            'visitors'  => $model->getvisitors(),
+            'title' => 'Visitor Entries',
         ];
 
         return view('templates/header', $data)
             . view('visitors/index')
             . view('templates/footer');
     }
-	
-	public function join()
+
+    public function view($slug = null)
+    {
+        $model = model(VisitorsModel::class);
+
+        $data['visitors'] = $model->getVisitors($slug);
+
+        if (empty($data['visitors'])) {
+            throw new PageNotFoundException('Cannot find the visitors entry: ' . $slug);
+        }
+
+        $data['title'] = $data['visitors']['title'];
+
+        return view('templates/header', $data)
+            . view('visitors/view')
+            . view('templates/footer');
+    }
+
+    public function create()
     {
         helper('form');
 
         // Checks whether the form is submitted.
         if (! $this->request->is('post')) {
             // The form is not submitted, so returns the form.
-            return view('templates/header', ['title' => 'Create Visitors Entry'])
-                . view('visitors/join')   
+            return view('templates/header', ['title' => 'Create a visitors entry'])
+                . view('visitors/create')
                 . view('templates/footer');
         }
 
@@ -36,11 +55,11 @@ class Visitors extends BaseController
         // Checks whether the submitted data passed the validation rules.
         if (! $this->validateData($post, [
             'fname' => 'required|max_length[255]|min_length[3]',
-            'message' => 'required|max_length[255]|min_length[3]',		
+            'message'  => 'required|max_length[1000]|min_length[1]',
         ])) {
             // The validation fails, so returns the form.
-            return view('templates/header', ['title' => 'Create Visitors Entry'])
-                . view('visitors/join')
+            return view('templates/header', ['title' => 'Create a visitors entry'])
+                . view('visitors/create')
                 . view('templates/footer');
         }
 
@@ -48,11 +67,12 @@ class Visitors extends BaseController
 
         $model->save([
             'fname' => $post['fname'],
-            'message' => $post['mesasge'],
+            'slug'  => url_title($post['fname'], '-', true),
+            'message'  => $post['message'],
         ]);
 
-        return view('templates/header', ['title' => 'Create Visitors Entry'])
+        return view('templates/header', ['title' => 'Create a visitors entry'])
             . view('visitors/success')
             . view('templates/footer');
     }
-}	
+}
